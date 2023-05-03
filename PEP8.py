8ba8a995fff736306ec54ad5071d01dcfc8d8ad7
Nestor <delarosa.nestor@uabc.edu.mx>
2023-05-02 19:43:07 -0700

    mi primer commit

---
+++ PEP8.py
@@ -0,0 +1,11 @@
+class user():
+    def __init__(self,username,password=''):
+    
+        self.username = username
+
+        self.password = password
+
+        def set__password(self):
+            pass
+
+        codyuser = user('cody')
